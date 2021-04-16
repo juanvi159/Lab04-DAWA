da1fb3bc14894b055d0628a5f5cf71537aceb128
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-04-15 19:00:30 -0500

    anecdotas 2 Agregar Votos

--- anecdotas/src/index.js
+++ anecdotas/src/index.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
+const App = () => {
+  const [selected, setSelected] = useState(anecdotas[Math.floor(Math.random() * anecdotas.length)])
+  const [votos, setVotos] = useState(0)
 
+  function cambiarAnecdota() {
+    const anecdota = anecdotas[Math.floor(Math.random() * anecdotas.length)];
+    setSelected(anecdota);
+    setVotos(anecdota.votos);
+  }
   return (
-    <div>
-      {props.anecdotes[selected]}
+    <div> 
+      <h4>{selected.anectoda}</h4>
+      <h3>Tiene: {votos} Votos</h3>
+      <button onClick={()=>{selected.votos++;setVotos(selected.votos)}}>Votar</button>
+      <button onClick={cambiarAnecdota} >Siguiente Anecdota</button>
     </div>
   )
 }
 
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+const anecdotas = [
+  {anectoda:'La tripulacion miraba con Horror como una bestia me tragaba, Y no hizo nada',votos:0,},
+  {anectoda:'El verdadero sonido de mi espada, solo puede oirse cuando dominan la reencarnacion',votos:0,},
+  {anectoda:'El fin justifica la violencia de los medios',votos:0,},
+  {anectoda:'Me gusta programar, pero casi siempre me olvido de documentar mi codigo XD',votos:0,},
+  {anectoda:'Hacer algo, es mejor, que no hacer nada',votos:0,},
+  {anectoda:'No importa cuanto tiempo tengas, sino, como lo usas',votos:0,}
 ]
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App anecdotas={anecdotas} />,
   document.getElementById('root')
 )
